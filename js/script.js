61545b0daf97a97606484ea442da1d025e2fc911
bobgdotnet <95641422+bobgdotnet@users.noreply.github.com>
2024-09-27 15:03:51 -0700

    assignment1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,107 @@
-// STEP 1
+/* STEP 1
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let MyLibraryFunction
 // STEP 2
+let numberLiteral = 5
+let booleanLiteral = true
+let stringLiteral = 'true'
+let nullLiteral = null
 // STEP 3
+let complexEx1 = 2 + 2
+let complexEx2 = 'foo' + 'bar'
+// STEP 4
+let strFirstName
+let strLastName
+let strAddress
+let strCity
+let strState
+let nZipCode
+let nYourAge
+let strReferralSource
+let bMayWeContactYou
+// STEP 5
+let strFirstName
+let strLastName
+let strState
+strFirstName = "John"
+strLastName = "Doe"
+strState "CA"
+
+let strFirstName = "John"
+let strLastName = "Doe"
+let strState = "CA"
+
+let strFirstName = "John", let strLastName = "Doe", let strState = "CA"
+// STEP 6
+let mixedVar = "The answer is " + 42
+console.log(mixedVar)
+// STEP 7
+let bstrFoo = "too " + true
+let nbBar = 42 + false
+console.log(bstrFoo)
+console.log(nbBar)
+// STEP 8
+    //need to escape the quotation marks
+let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+console.log(someString)
+// STEP 9
+let nullVal = null
+let undVal
+console.log(nullVal)
+console.log(undVal) 
+// STEP 10
+typeof true
+typeof 42
+typeof "string"
+typeof Object
+typeof undVal
+// STEP 11
+alert("Hello " + "Robert Gally" + ", welcome to the JavaScript class!")
+// STEP 12
+let strName = "Your Name"
+alert("Hello " + strName + ", welcome to the JavaScript class!")
+// STEP 13
+let course = "JavaScript"
+alert("Hello " + strName + ", welcome to the " + course + " class!")
+// STEP 14
+alert("Hello " + strName + ".\nWelcome to the " + course + " class!")
+// STEP 15
+let course = "JavaScript"
+let usersName = prompt("What is your name?")
+alert("Hello " + usersName + ".\nWelcome to the " + course + " class!")
+// STEP 16
+let course = prompt("What class are you taking?")
+alert("Hello " + usersName + ".\nWelcome to the " + course + " class!")
+// STEP 17
+let x = 10, y = 20
+console.log(x + y)
+// STEP 18
+let x = 20
+x = x+20
+console.log(x)
+// STEP 19
+let x = 20
+x = x*5
+console.log(x)
+// STEP 20
+let x = 20 % 3
+console.log(x)
+// step 21
+let z
+let x = 1
+let y = 1
+if (x === y) 
+    z = true
+console.log(z)
+// STEP 22
+let z
+let x = 1
+let y = 1
+if (x !== y) 
+    z = true
+else 
+    z = false
+console.log(z)
